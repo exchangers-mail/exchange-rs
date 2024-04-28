c15277d2f4cb7e48bf8a620e11016c39b934dde6
Dom Rodriguez <shymega@shymega.org.uk>
2024-04-28 23:06:53 +0100

    refactor: Fix all compile and Cargo errors
    WIP.

--- src/lib.rs
+++ src/lib.rs
@@ -6,39 +6,15 @@ pub enum ExchangeApiKind {
 pub enum ExchangeServerKind {
     Exchange2016(Vec<ExchangeApiKind>),
     Exchange2019(Vec<ExchangeApiKind>),
-    ExchangeOnline(ExchangeApiKind::Graph),
+    ExchangeOnline(ExchangeApiKind),
 }
 
 pub enum ExchangeServer {
     OnPremise(ExchangeServerKind),
-    Online(ExchangeServerKind::ExchangeOnline),
+    Online(ExchangeServerKind),
 }
 
-pub struct<T = ExchangeServer> ExchangeConnection<T> {
+pub struct ExchangeConnection<T = ExchangeServer> {
     server_type: T,
     connected: bool,
-}
-
-impl ExchangeConnection<Target = ExchangeServer> {
-    fn is_online(&self) -> bool;
-    fn is_connected(&self) -> bool;
-    fn is_cloud(&self) -> bool {
-        self.is_online()
-    }
-    fn is_on_premise(&self) -> bool {
-        !self.is_online()
-    }
-    fn is_onpremise(&self) -> bool {
-        self.is_on_premise()
-    }
-
-    fn raw(&self) -> Option<String> {
-        None
-    }
-    fn is_parsed(&self) -> bool {
-        false
-    }
-    fn parse(&self) -> Option<String> {
-        None
-    }
 }
