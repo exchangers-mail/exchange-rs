8f730009d784a6877c9356b98675a59089efc02a
Dom Rodriguez <shymega@shymega.org.uk>
2024-06-11 23:59:53 +0100

    feat: Add Nixified `devenv` support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{
+  languages = {
+    nix.enable = true;
+    rust.enable = true;
+    shell.enable = true;
+  };
+  devcontainer.enable = true;
+  difftastic.enable = true;
+  dotenv.enable = true;
+  pre-commit.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+    clippy.settings.offline = false;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    actionlint.enable = true;
+    nixpkgs-fmt.enable = true;
+    markdownlint.enable = true;
+    statix.enable = true;
+  };
+}
